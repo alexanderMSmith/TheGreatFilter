ec6563c512b03352ce871f1273a1fbd17acdaaf4
Alexander Smith <109933022+alexanderMSmith@users.noreply.github.com>
2026-08-04 12:57:20 -0700

    Update CharacterCreator.java

--- src/main/java/CharacterCreator.java
+++ src/main/java/CharacterCreator.java
@@ -1,19 +1,18 @@
 import java.util.List;
 
 public class CharacterCreator {
-    public static String createCharacter(int raceRange, int featRange, int backgroundRange,
-        int languagePriorityRange, int languageRange, int statPriorityRange, int statRange) {
-        String race = TraitsSelector.getRace(raceRange);
-        List<String> characterFeats = TraitsSelector.feats(featRange);
-        String background = TraitsSelector.getBackground(backgroundRange);
+    public static String createCharacter(CharacterSettings chara) {
+        String race = TraitsSelector.getRace(chara.getRaceRange());
+        List<String> characterFeats = TraitsSelector.feats(chara.getFeatRange());
+        String background = TraitsSelector.getBackground(chara.getFeatRange());
         List<Integer> rolledStats = StatRoller.rollStats();
         String message = printStats(rolledStats);
         message += "\nFeat: " + characterFeats.toString() + "\nBackground: " + background;
         message += "\nRace: " + race;
         message += "\nStat Priority: " + TraitsSelector.getStatPriority
-            (statPriorityRange, statRange).toString();
+            (chara.getStatPriorityRange(), chara.getStatRange()).toString();
         message += "\nLanguage Priority " + TraitsSelector.getLanguagePriority
-            (languagePriorityRange, languageRange).toString();
+            (chara.getLanguagePriorityRange(), chara.getLanguageRange()).toString();
         return message;
     }
 
@@ -25,7 +24,8 @@ public class CharacterCreator {
     }
 
     public static void main(String[] args){
-        String character = createCharacter(225, 125, 45, 3, 15, 3, 6);
+        CharacterSettings chara = new CharacterSettings(225, 125, 45, 3, 15, 3, 6);
+        String character = createCharacter(chara);
         System.out.println(character);
     }
 }
